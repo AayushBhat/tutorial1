543eb9a4332586a4c0e35553886645a891bcf352
Aayush Bhat <aayush.bhat@mail.mcgill.ca>
2020-09-15 21:52:22 -0400

    student attribute name added

--- src/tutorial1/Student.java
+++ src/tutorial1/Student.java
@@ -1,5 +1,20 @@
 package tutorial1;
 
 public class Student {
+	
+	String name;
+	
+	public Student() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String studentName) {
+		this.name = studentName;
+	}
+	
 
 }
